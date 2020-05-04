674f3341e8c0aa5ee19a70f316f08d0a4edb3cab
gregalantiu2 <tiugreg2@gmail.com>
2020-05-03 22:49:32 -0400

    Get actions added to controller

--- DogWalking.API/Controllers/DogsController.cs
+++ DogWalking.API/Controllers/DogsController.cs
@@ -6,8 +6,19 @@ using System.Threading.Tasks;
 
 namespace DogWalking.API.Controllers
 {
+    [ApiController]
+    [Route("api/Dogs")]
     public class DogsController : ControllerBase
     {
-
+        [HttpGet]
+        public JsonResult GetDogs()
+        {
+            return new JsonResult(DogsDummyDataStore.Current.Dogs);
+        }
+        [HttpGet("{id}")]
+        public JsonResult GetDog(int id)
+        {
+            return new JsonResult(DogsDummyDataStore.Current.Dogs.FirstOrDefault(d => d.ID == id));
+        }
     }
 }
---
+++ DogWalking.API/DogsDummyDataStore.cs
@@ -0,0 +1,31 @@
+﻿using DogWalking.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DogWalking.API
+{
+    public class DogsDummyDataStore
+    {
+        public static DogsDummyDataStore Current { get; } = new DogsDummyDataStore();
+        public List<DogDTO>  Dogs { get; set;}
+        public DogsDummyDataStore()
+        {
+            Dogs = new List<DogDTO>()
+            {
+                new DogDTO()
+                {
+                    ID = 1,
+                    Name = "Libby"
+                },
+                new DogDTO()
+                {
+                    ID = 2,
+                    Name = "Harlan"
+                }
+            };
+        }
+
+    }
+}
